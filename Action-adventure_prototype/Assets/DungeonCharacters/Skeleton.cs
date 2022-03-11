7ab1eba91304116102592a02252cea479f8817f0
Ahreum999 <ahreum999@gmail.com>
2022-03-11 18:24:42 -0500

    Dungeon Character downloaded and linked to Enemy Behaviour
    *used NavMeshMap
    *Prefab wander between two points
    *Follow when player is in their boundry
    *Animation is sinked, looking towards the player is set!

---
+++ Action-adventure_prototype/Assets/DungeonCharacters/Skeleton.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Skeleton: MonoBehaviour
+{
+    [SerializeField]
+    private NavMeshAgent _agent;
+    [SerializeField]
+    private Animator _animator;
+    //range = distance between the skeleton and point 
+    [SerializeField]
+    private float _range = 5f;
+    // boundry: checking if player is in the boundry
+    [SerializeField]
+    private float _boundry = 5f;
+    private AiState _currentState;
+    public Transform Player;
+    public Transform _pointTogo;
+    public Transform _pointTocome;
+    public bool FollowingEnemy = false;
+    private bool _toA = true;
+    private bool _toB = false;
+    private enum AiState
+    {
+        Wandering,
+        Following
+    }
+    void Start()
+    {  
+        _agent = GetComponent<NavMeshAgent>();
+        _currentState = AiState.Wandering;
+        SetDestination(_pointTogo.transform.position);
+
+    }
+
+    void Update()
+    {
+        if (_currentState == AiState.Wandering)
+        {
+            _animator.SetBool("walking", true);
+            _animator.SetBool("attacking", false);
+
+
+            if (_agent.remainingDistance <= _range && _toA == true && _toB == false)
+            {
+                SetDestination(_pointTocome.position);
+                _toA = false;
+                _toB = true;
+            }
+            else if (_agent.remainingDistance <= _range && _toA == false && _toB == true)
+            {
+                
+                SetDestination(_pointTogo.transform.position);
+                _toA = true;
+                _toB = false;
+            }
+           
+            else if (Vector3.Distance(transform.position, Player.transform.position) < _boundry)
+            {
+                _currentState = AiState.Following;
+                _toA = false;
+                _toB = false;
+            } 
+        }
+
+        else if (_currentState == AiState.Following)
+        {
+            FollowingEnemy = true;
+            _animator.SetBool("attacking", true);
+            _animator.SetBool("walking", false);
+            SetDestination(Player.transform.position);
+            _toA = false;
+            _toB = false;
+            if (Vector3.Distance(transform.position, Player.transform.position) >= _boundry)
+            {
+                _currentState = AiState.Wandering;
+                FollowingEnemy = false;
+                _toA = true;
+                _toB = false;
+            }
+        }
+
+   
+    }
+
+    void SetDestination(Vector3 point)
+    {
+        _agent.SetDestination(point);
+        _agent.destination = point;
+        FaceTarget(point);
+    }
+
+    void FaceTarget(Vector3 target)
+    {
+        Vector3 direction = (target - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+    }
+
+}
+
